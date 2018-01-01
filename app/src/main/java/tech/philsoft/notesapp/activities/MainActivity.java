866074f783e15a07608b9fdb3a0132abb411f644
Phillip Nzuli <mwanikiphillip@gmail.com>
2018-01-01 19:07:33 +0300

    Moved MainActivity to activities package

--- app/src/main/java/tech/philsoft/notesapp/activities/MainActivity.java
+++ app/src/main/java/tech/philsoft/notesapp/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package tech.philsoft.notesapp;
+package tech.philsoft.notesapp.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import tech.philsoft.notesapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
