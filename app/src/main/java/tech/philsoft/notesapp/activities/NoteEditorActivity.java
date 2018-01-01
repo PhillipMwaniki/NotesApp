a13644ff4959dde24e5edda3c2734d803c85130f
Phillip Nzuli <mwanikiphillip@gmail.com>
2018-01-01 19:11:00 +0300

    Added NoteEditor Activity

---
+++ app/src/main/java/tech/philsoft/notesapp/activities/NoteEditorActivity.java
@@ -0,0 +1,15 @@
+package tech.philsoft.notesapp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import tech.philsoft.notesapp.R;
+
+public class NoteEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_editor);
+    }
+}
