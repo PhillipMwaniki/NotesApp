80e4c1ab444bfe1d891957c759cd84b44300e8b7
Phillip Nzuli <mwanikiphillip@gmail.com>
2018-01-02 08:50:59 +0300

    Added recycler view and support for material drawer

--- app/src/main/java/tech/philsoft/notesapp/activities/MainActivity.java
+++ app/src/main/java/tech/philsoft/notesapp/activities/MainActivity.java
@@ -11,6 +11,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.mikepenz.fontawesome_typeface_library.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.Nameable;
+import com.mikepenz.materialdrawer.util.KeyboardUtil;
 
 import tech.philsoft.notesapp.R;
 import tech.philsoft.notesapp.fragments.NotePlainEditorFragment;
@@ -18,6 +27,7 @@ import tech.philsoft.notesapp.fragments.NotePlainEditorFragment;
 public class MainActivity extends AppCompatActivity implements NotePlainEditorFragment.OnFragmentInteractionListener{
 
     Toolbar mToolbar;
+    Drawer result = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,63 @@ public class MainActivity extends AppCompatActivity implements NotePlainEditorFr
             NotePlainEditorFragment fragment = new NotePlainEditorFragment();
             openFragment(fragment, "Note Editor");
         }
+
+        //Now build the navigation drawer and pass the AccountHeader
+        result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(mToolbar)
+                .withActionBarDrawerToggle(true)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName(R.string.title_home).withIcon(FontAwesome.Icon.faw_home).withIdentifier(1),
+                        new PrimaryDrawerItem().withName(R.string.title_settings).withIcon(FontAwesome.Icon.faw_list).withIdentifier(2)
+
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int i, IDrawerItem drawerItem) {
+                        if (drawerItem != null && drawerItem instanceof Nameable){
+                            String name = ((Nameable)drawerItem).getName().getText(MainActivity.this);
+                            mToolbar.setTitle(name);
+                        }
+
+                        if (drawerItem != null){
+                            int selectedScren = drawerItem.getIdentifier();
+                            switch (selectedScren){
+                                case 1:
+                                    //do nothing
+                                    break;
+                                case 2:
+                                    //go to settings screen, yet to be added
+                                    //this will be your home work
+                                    Toast.makeText(MainActivity.this, "Settings Clicked", Toast.LENGTH_SHORT).show();
+                                    break;
+                            }
+                        }
+                        return false;
+                    }
+
+
+                })
+                .withOnDrawerListener(new Drawer.OnDrawerListener() {
+                    @Override
+                    public void onDrawerOpened(View view) {
+                        KeyboardUtil.hideKeyboard(MainActivity.this);
+
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View view) {
+
+                    }
+
+                    @Override
+                    public void onDrawerSlide(View view, float v) {
+
+                    }
+                })
+                .withFireOnInitialOnClick(true)
+                .withSavedInstance(savedInstanceState)
+                .build();
 
     }
 
--- app/src/main/java/tech/philsoft/notesapp/fragments/NoteListFragment.java
+++ app/src/main/java/tech/philsoft/notesapp/fragments/NoteListFragment.java
@@ -1,71 +1,53 @@
 package tech.philsoft.notesapp.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import tech.philsoft.notesapp.R;
+import tech.philsoft.notesapp.activities.NoteEditorActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link NoteListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link NoteListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class NoteListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private FloatingActionButton mFab;
+    private View mRootView;
 
     private OnFragmentInteractionListener mListener;
 
-    public NoteListFragment() {
-        // Required empty public constructor
-    }
+    public NoteListFragment() {}
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NoteListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NoteListFragment newInstance(String param1, String param2) {
+    public static NoteListFragment newInstance() {
         NoteListFragment fragment = new NoteListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
+
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        mRootView = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        mFab = mRootView.findViewById(R.id.fab);
+        mFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(), NoteEditorActivity.class));
+            }
+        });
+
+        return mRootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
